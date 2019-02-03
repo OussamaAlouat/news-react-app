a0019229878ebb5570cb9544d8cfccf3df7d32f8
OussamaAlouat <al317257@uji.es>
2019-02-03 16:33:15 +0100

    Added ArchivedComponentDetails

---
+++ src/components/ArchivedComponentDetails.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import  moment from 'moment';
+
+const ArchivedComponentDetails = (props) => {
+
+    const doc = props.document;
+
+    return (
+        <div className="card">
+            <div className="card-header">
+                {doc.title}
+            </div>
+            <div className="card-body">
+                <h6 className="card-title">{doc.description}</h6>
+                <p className="card-text">{doc.content}</p>
+
+            </div>
+            <div className="card-footer text-muted">
+                <div className="row">
+                    <div className="col-4">
+                        Author: {doc.author}
+                    </div>
+                    <div className="col-4">
+                        Date: {moment(doc.date).format('YYYY-MM-DD HH:mm:SS')}
+                    </div>
+                    <div className="col-4">
+                        <button type="button" className="btn btn-secondary">Remove</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ArchivedComponentDetails;
