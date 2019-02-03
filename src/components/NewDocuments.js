56853de76f3cf8f38b9e6f90d2730334528642fb
OussamaAlouat <al317257@uji.es>
2019-02-03 20:15:59 +0100

    Modified the Link NewDocuments component

--- src/components/NewDocuments.js
+++ src/components/NewDocuments.js
@@ -19,9 +19,14 @@ const NewDocuments = (props) => {
             <div className="shadow-lg p-3 mb-3 bg-white rounded">
                 <h1 className="text-center">Welcome to your news</h1>
             </div>
+            <div className="row justify-content-center">
+                <div className="col-2 justify-content-center mb-3 ">
+                    <Link to="/new" className="badge badge-primary">GO TO ADD NEWS</Link>
+                </div>
 
-            <div className="row justify-content-center mb-3">
-                <Link to="/archived" className="badge badge-primary">GO TO ARCHIVE</Link>
+                <div className="col-2 justify-content-center mb-3">
+                    <Link to="/archived" className="badge badge-primary">GO TO ARCHIVE NEWS</Link>
+                </div>
             </div>
 
             {documentsList}
