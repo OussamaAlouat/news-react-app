a548af25dcd838069c6d445631c24a8af44f4e45
OussamaAlouat <al317257@uji.es>
2019-02-03 17:27:07 +0100

    Added callback function to get from child  the  document and pass to parent this document

--- src/components/ArchivedDocuments.js
+++ src/components/ArchivedDocuments.js
@@ -3,7 +3,16 @@ import ArchivedComponentDetails from "./ArchivedComponentDetails";
 
 const ArchivedDocuments = (props) => {
 
-    const documentsList = props.documents.map((doc) => <ArchivedComponentDetails key={doc._id} document={doc}/>)
+    const onDocumentRemove = (item) => {
+        props.onDocumentRemove(item);
+    };
+
+    const documentsList = props.documents.map((doc) =>
+        <ArchivedComponentDetails
+            key={doc._id}
+            document={doc}
+            onDocumentRemove={onDocumentRemove}/>);
+
 
     return (
         <div>
