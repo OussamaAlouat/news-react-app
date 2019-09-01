084d61b12c652812f61c3b203d004cde69b1a5fe
OussamaAlouat <al317257@uji.es>
2019-09-01 14:35:42 +0200

    Refactore code and change space tab to 2, on index

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDOM.render(
-        <App/>,
-    document.querySelector('#root')
+  <App/>,
+  document.querySelector('#root')
 );
